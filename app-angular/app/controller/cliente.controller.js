12ea998d9deb04dd0924a94d7c04179e20bb8b12
LeeUmpierre <umpierre2507@hotmail.com>
2020-05-14 21:58:18 -0300

    Aula 14/05/2020

--- app-angular/app/controller/cliente.controller.js
+++ app-angular/app/controller/cliente.controller.js
@@ -1,20 +1,77 @@
-(function( app ){
+//controla Js dentro de um escopo
+(function (app) {
     'use strict';
-    
-    app.controller('ClienteController', function( $scope, ClienteService ){
- 
-     //Carrega uma lista de clientes
-     ClienteService.listar().then(function( result ){
-         $scope.clientes = result.data;
-     }).catch(function(error) {
-         if (error.status == 404) {
-             $scope.msgerro = 'O recurso não foi encontrado';
-         } else {
-             console.log(error);
-         }
-         
-     });
- 
+
+    app.controller('ClienteController', function ($scope, ClienteService) {
+
+        $scope.filterColumn = 'nome';
+        $scope.setFilterColumn = function (columnName) {
+            $scope.filterColumn = columnName;
+        }
+        $scope.filter = function () {
+            var filtro = {};
+            filtro[filterColumn] = textFilter;
+
+
+            return filtro;
+        }
+
+        //Carrega uma lista de clientes
+        //Promise
+        //then = operacao finalizada
+        ClienteService.listar().then(function (result) {//controla Js dentro de um escopo
+            (function (app) {
+                'use strict';
+
+                app.controller('ClienteController', function ($scope, ClienteService) {
+
+                    $scope.decrescente = false;
+                    $scope.selectedColumn = 'nome';
+
+                    $scope.setColumn = function (columnName) {
+                        $scope.selectedColumn = columnName;
+
+                        //determina o ordenação decrescente (false)
+                        $scope.decrescente = !$scope.decrescente;
+                    }
+
+                    $scope.filter = function () {
+                        var filtro = {};
+
+                        filtro[$scope.selectedColumn] = $scope.textFilter;
+
+                        return filtro;
+                    }
+
+                    //Carrega uma lista de clientes
+
+                    //Promise
+                    //then = operacao finalizada
+                    ClienteService.listar().then(function (result) {
+                        $scope.clientes = result.data;
+                    }).catch(function (error) {
+                        if (error.status == 404) {
+                            $scope.msgerro = 'O recurso não foi encontrado';
+                        } else {
+                            $scope.msgerro = error.statusText;
+                        }
+
+                    });
+
+                });
+
+            })(appJS);
+            $scope.clientes = result.data;
+            //catch = pega erro
+        }).catch(function (error) {
+            if (error.status == 404) {
+                $scope.msgerro = 'O recurso não foi encontrado';
+            } else {
+                $scope.msgerro = error.statusText;
+            }
+
+        });
+
     });
-    
- })( appJS );
+
+})(appJS);
--- app-angular/app/service/cliente.service.js
+++ app-angular/app/service/cliente.service.js
@@ -1,11 +1,15 @@
+//provedor de dados
 (function( app ){
     'use strict';
 
     app.service('ClienteService', function( $http ) {
 
         function loadJSON() {
+	    //mockaroo
+            //return $http.get('https://my.api.mockaroo.com/clientes.json?key=d66f5df0');
             return $http.get('data/clientes.json');
         }
+        
         return {
             listar: loadJSON
         }
